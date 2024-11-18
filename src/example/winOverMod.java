250871d51ec142b99b685914687b7a66858b32fc
Ancvike <2047870696@qq.com>
2024-11-18 14:22:49 +0800

    winOverMod提交测试

--- src/example/winOverMod.java
+++ src/example/winOverMod.java
@@ -13,7 +13,7 @@ public class winOverMod extends Mod {
     public winOverMod() {
         //Events.on侦听事件是否触发,e为事件触发后进行动作
         //SectorCaptureEvent为区域占领后触发事件
-        Events.on(SectorCaptureEvent.class, e -> {
+        Events.on(WinEvent.class, e -> {
             BaseDialog dialog = new BaseDialog("frog");
             dialog.cont.add("behold").row();
             dialog.cont.image(Core.atlas.find("example-java-mod-frog")).pad(20f).row();
