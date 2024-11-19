92672371b770eb41af01506454240066735796cc
Ancvike <2047870696@qq.com>
2024-11-19 19:32:12 +0800

    提交测试

--- src/Code/FullResource/FullResource.java
+++ src/Code/FullResource/FullResource.java
@@ -1,8 +1,15 @@
 package Code.FullResource;
 
+import arc.Core;
 import mindustry.Vars;
+import mindustry.ui.dialogs.BaseDialog;
 
 public class FullResource {
     public FullResource() {
+        BaseDialog dialog = new BaseDialog("frog");
+        dialog.cont.add("behold").row();
+        dialog.cont.image(Core.atlas.find("example-java-mod-frog")).pad(20f).row();
+        dialog.cont.button("I see", dialog::hide).size(100f, 50f);
+        dialog.show();
     }
 }
