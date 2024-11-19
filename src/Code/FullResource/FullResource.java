3e0db4e0ef26f23f1878b8fafddc35715ecfce42
Ancvike <2047870696@qq.com>
2024-11-19 15:25:58 +0800

    提交测试

--- src/Code/FullResource/FullResource.java
+++ src/Code/FullResource/FullResource.java
@@ -1,34 +1,9 @@
 package Code.FullResource;
 
-import arc.Core;
-import arc.Events;
-import arc.util.Time;
-import mindustry.game.EventType;
-import mindustry.ui.dialogs.BaseDialog;
+import mindustry.Vars;
 
 public class FullResource {
     public FullResource() {
-        Events.on(EventType.ClientLoadEvent.class, e -> {
-            //show dialog upon startup
-            Time.runTask(10f, () -> {
-                BaseDialog dialog = new BaseDialog("frog");
-                dialog.cont.add("loading").row();
-                //mod sprites are prefixed with the mod name (this mod is called 'example-java-mod' in its config)
-                dialog.cont.image(Core.atlas.find("Code-java-mod-frog")).pad(20f).row();
-                dialog.cont.button("I see", dialog::hide).size(100f, 50f);
-                dialog.show();
-            });
-        });
-        Events.on(EventType.SectorCaptureEvent.class, e -> {
-            //show dialog upon startup
-            Time.runTask(10f, () -> {
-                BaseDialog dialog = new BaseDialog("frog");
-                dialog.cont.add("behold").row();
-                //mod sprites are prefixed with the mod name (this mod is called 'example-java-mod' in its config)
-                dialog.cont.image(Core.atlas.find("Code-java-mod-frog")).pad(20f).row();
-                dialog.cont.button("I see", dialog::hide).size(100f, 50f);
-                dialog.show();
-            });
-        });
+        Vars.ui.hudfrag.showToast("Ancvike's modding");
     }
 }
