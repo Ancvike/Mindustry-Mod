e540aecff25b19bd1c3690c0501ff5e37eba2885
Ancvike <2047870696@qq.com>
2024-11-20 09:16:11 +0800

    提交测试

--- src/Code/FullResource/FullResource.java
+++ src/Code/FullResource/FullResource.java
@@ -1,19 +1,9 @@
 package Code.FullResource;
 
 import mindustry.content.SectorPresets;
-import static mindustry.content.Planets.serpulo;
-import mindustry.type.SectorPreset;
 
 public class FullResource extends SectorPresets {
     public FullResource() {
-        groundZero = new SectorPreset("groundZero", serpulo, 15){{
-            alwaysUnlocked = true;
-            addStartingItems = true;
-            captureWave = 1;
-            difficulty = 1;
-            overrideLaunchDefaults = true;
-            noLighting = true;
-            startWaveTimeMultiplier = 3f;
-        }};
+        groundZero.captureWave = 1;
     }
 }
