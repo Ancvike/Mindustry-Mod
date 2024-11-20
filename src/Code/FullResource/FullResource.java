58c07d807bb50ee88638699edb571cab548523dc
Ancvike <2047870696@qq.com>
2024-11-20 20:36:42 +0800

    提交测试

--- src/Code/FullResource/FullResource.java
+++ src/Code/FullResource/FullResource.java
@@ -1,13 +1,7 @@
 package Code.FullResource;
 
-import arc.Events;
-import mindustry.content.SectorPresets;
-import mindustry.game.EventType;
-
-public class FullResource extends SectorPresets {
+public class FullResource{
     public FullResource() {
-        for (;;) {
-            groundZero.captureWave = 1;
-        }
+        
     }
 }
