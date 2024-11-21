94f5f65c826902695af9fa05f64a4a335ca8c509
Ancvike <2047870696@qq.com>
2024-11-21 18:06:48 +0800

    提交测试

--- src/Code/FullResource.java
+++ src/Code/FullResource.java
@@ -1,24 +1,7 @@
 package Code;
 
-import Code.ui.fragments.sidebar.windows.tools.draws.OverDrawManager;
-import Code.ui.fragments.sidebar.windows.tools.tools.ToolManager;
-import Code.ui.fragments.sidebar.dialogs.DialogManager;
-import Code.ui.fragments.sidebar.windows.*;
-import Code.ui.fragments.FragmentManager;
-import mindustry.game.EventType.*;
-import mindustry.mod.*;
-import arc.*;
+import mindustry.mod.Mod;
 
 public class FullResource extends Mod {
-    public void init() {
-        Events.on(ClientLoadEvent.class, e -> {
-            //
-            WindowManager.init();
-            DialogManager.init();
-            FragmentManager.init();
-            OverDrawManager.init();
-            ToolManager.init();
-            SVars.init();
-        });
-    }
+
 }
