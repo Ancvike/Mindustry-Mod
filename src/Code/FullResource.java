f403f4c1b0db478b123b0181f54bed1b931bd55d
Ancvike <2047870696@qq.com>
2024-11-22 20:25:40 +0800

    提交

--- src/Code/FullResource.java
+++ src/Code/FullResource.java
@@ -1,7 +1,29 @@
 package Code;
 
+import arc.scene.ui.Button;
+import arc.scene.ui.Slider;
+import arc.scene.ui.layout.Table;
 import mindustry.mod.Mod;
+import mindustry.ui.dialogs.BaseDialog;
 
-public class FullResource extends Mod {
-    //
+public class FullResource extends BaseDialog {
+    public FullResource() {
+        super("设置");
+
+        Table table = new Table();
+        Slider speedSlider = new Slider(1, 10, 1, false);
+        Button applyButton = new Button();
+
+        applyButton.addListener(event -> {
+            float selectedSpeed = speedSlider.getValue();
+            // 处理选中速度
+            return true;
+        });
+
+        table.add(speedSlider).fillX().pad(10);
+        table.row();
+        table.add(applyButton).pad(10);
+        add(table).pad(10);
+        pack();
+    }
 }
