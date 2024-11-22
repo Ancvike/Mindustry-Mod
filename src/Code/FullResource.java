40dd61991089a04c6b44a12c06cf99c9760b83d1
Ancvike <2047870696@qq.com>
2024-11-22 21:15:50 +0800

    提交

--- src/Code/FullResource.java
+++ src/Code/FullResource.java
@@ -6,7 +6,6 @@ import mindustry.mod.Mod;
 
 public class FullResource extends Mod {
     public FullResource() {
-
         Events.on(EventType.ClientLoadEvent.class, e -> {
             new ui().show();
         });
