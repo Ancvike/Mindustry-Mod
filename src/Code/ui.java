c80950c2795da1a46c7140305e9f1f079c01a31e
Ancvike <2047870696@qq.com>
2024-11-22 20:32:46 +0800

    提交

---
+++ src/Code/ui.java
@@ -0,0 +1,27 @@
+package Code;
+
+import arc.scene.ui.Button;
+import arc.scene.ui.Slider;
+import arc.scene.ui.layout.Table;
+import mindustry.ui.dialogs.BaseDialog;
+
+public class ui extends BaseDialog {
+    public ui() {
+        super("设置");
+        Table table = new Table();
+        Slider speedSlider = new Slider(1, 10, 1, false);
+        Button applyButton = new Button();
+
+        applyButton.addListener(event -> {
+            float selectedSpeed = speedSlider.getValue();
+            // 处理选中速度
+            return true;
+        });
+
+        table.add(speedSlider).fillX().pad(10);
+        table.row();
+        table.add(applyButton).pad(10);
+        add(table).pad(10);
+        pack();
+    }
+}
