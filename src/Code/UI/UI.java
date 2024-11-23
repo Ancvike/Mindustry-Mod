54ddc8743e93696a379da33f217353430ffc4be0
Ancvike <2047870696@qq.com>
2024-11-23 19:50:47 +0800

    测试

--- src/Code/UI/UI.java
+++ src/Code/UI/UI.java
@@ -1,8 +1,21 @@
 package Code.UI;
 
+import arc.Core;
+import arc.scene.ui.ImageButton;
+import mindustry.Vars;
+
 public class UI {
+    ImageButton imageButton = new ImageButton(Core.atlas.find("Code-java-mod-frog"));
 
     public UI() {
+        Vars.ui.hudGroup.fill(t -> {
+            t.add(imageButton).size(100,100);
+            t.right();
+            t.top();
+        });
+    }
+
+    public void onClick() {
 
     }
 }
