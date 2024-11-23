49d07569a9012076fe706b852dc1179e190a184d
Ancvike <2047870696@qq.com>
2024-11-23 13:30:47 +0800

    测试

--- src/Code/UI/UI.java
+++ src/Code/UI/UI.java
@@ -1,5 +1,22 @@
 package Code.UI;
 
-public class UI {
+import arc.util.Time;
+import mindustry.Vars;
+import mindustry.ui.dialogs.BaseDialog;
 
+public class UI {
+    public UI() {
+        Vars.ui.menuGroup.fill(c -> {
+            c.bottom().right();
+            c.button("点我", () -> {
+                Vars.ui.loadfrag.show();
+                Time.runTask(10f, () -> {
+                    Vars.ui.loadfrag.hide();
+                });
+                BaseDialog baseDialog = new BaseDialog("按钮测试页");
+                baseDialog.addCloseButton();
+                baseDialog.show();
+            }).size(200, 60).padBottom(45);
+        });
+    }
 }
