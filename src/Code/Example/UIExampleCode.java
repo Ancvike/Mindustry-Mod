3bc70f7947c4d1e33567d0db159d3951ce69abbb
Ancvike <2047870696@qq.com>
2024-11-23 12:27:21 +0800

    测试

--- src/Code/Example/UIExampleCode.java
+++ src/Code/Example/UIExampleCode.java
@@ -1,4 +1,27 @@
 package Code.Example;
 
+import arc.scene.ui.Slider;
+import arc.scene.ui.TextButton;
+import arc.scene.ui.layout.Table;
+import mindustry.ui.dialogs.BaseDialog;
+
+import java.util.Objects;
+
 public class UIExampleCode {
+//    BaseDialog baseDialog = new BaseDialog("测试页");
+//    Table table = new Table();
+//    Slider speedSlider = new Slider(1.0F, 10.0F, 1.0F, false);
+//    TextButton exitButton = new TextButton("退出测试");
+//
+//    public UI() {
+//        this.table.add(this.speedSlider).fillX().pad(10.0F);
+//        this.baseDialog.cont.add(this.exitButton).pad(10.0F);
+//        this.baseDialog.add(this.table);
+//        this.baseDialog.addCloseButton();
+//        this.baseDialog.show();
+//        TextButton var10000 = this.exitButton;
+//        BaseDialog var10001 = this.baseDialog;
+//        Objects.requireNonNull(var10001);
+//        var10000.clicked(var10001::hide);
+//    }
 }
