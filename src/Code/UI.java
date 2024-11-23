4cce4bb543cb474c3f0bab51cd80fe06c04536e6
Ancvike <2047870696@qq.com>
2024-11-23 10:57:46 +0800

    提交

--- src/Code/UI.java
+++ src/Code/UI.java
@@ -21,8 +21,8 @@ public class UI {
         table.add(speedSlider).fillX().pad(10);  // 添加滑块，填充宽度并加一些边距
         // 创建 "退出测试" 按钮
         TextButton exitButton = new TextButton("退出测试");
-        exitButton.clicked(r);
         baseDialog.cont.add(exitButton).pad(10); // 添加退出按钮到对话框
         baseDialog.show();
+        exitButton.clicked(r);
     }
 }
