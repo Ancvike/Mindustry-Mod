dbb4e14d8725b791c4bbfddda950bbe53566321d
Ancvike <2047870696@qq.com>
2024-11-23 11:32:43 +0800

    提交

--- src/Code/CodeExplanation/CodeExplanation.java
+++ src/Code/CodeExplanation/CodeExplanation.java
@@ -1,6 +1,7 @@
 package Code.CodeExplanation;
 
 public class CodeExplanation {
+    //原作者示例代码
 //        Log.info("Loaded ExampleJavaMod constructor.");
 //            //listen for game load event
 //        Events.on(EventType.ClientLoadEvent .class, e -> {
@@ -18,5 +19,20 @@ public class CodeExplanation {
 //    public void loadContent(){
 //        Log.info("Loading some example content.");
 //    }
+    //我的UI测试代码
+//    BaseDialog baseDialog = new BaseDialog("测试页");
+//    Table table = new Table();  // 创建一个表格用于布局
+//    Slider speedSlider = new Slider(1, 10, 1, false);  // 创建滑块，范围从1到10，步长为1
+//    TextButton exitButton = new TextButton("退出测试");
+//
+//    public UI() {
+//        table.add(speedSlider).fillX().pad(10);
+//        baseDialog.cont.add(exitButton).pad(10);// 将滑块添加到表格，填充宽度并加一些边距
+//        baseDialog.add(table);
+//        baseDialog.addCloseButton();  // 添加关闭按钮
+//        // 显示对话框
+//        baseDialog.show();
+//        exitButton.clicked(baseDialog::hide);
+//  }
 
 }
--- src/Code/UI.java
+++ src/Code/UI.java
@@ -1,38 +1,5 @@
 package Code;
 
-import arc.scene.ui.Slider;
-import arc.scene.ui.TextButton;
-import arc.scene.ui.layout.Table;
-import mindustry.ui.dialogs.BaseDialog;
-
 public class UI {
-    BaseDialog baseDialog = new BaseDialog("测试页");
-    Table table = new Table();  // 创建一个表格用于布局
-    Slider speedSlider = new Slider(1, 10, 1, false);  // 创建滑块，范围从1到10，步长为1
-    TextButton exitButton = new TextButton("退出测试");
 
-    public UI() {
-        table.add(speedSlider).fillX().pad(10);
-        baseDialog.cont.add(exitButton).pad(10);// 将滑块添加到表格，填充宽度并加一些边距
-        baseDialog.add(table);
-        baseDialog.addCloseButton();  // 添加关闭按钮
-        // 显示对话框
-        baseDialog.show();
-        exitButton.clicked(baseDialog::hide);
-//        BaseDialog baseDialog = new BaseDialog("测试页");
-//        Runnable r = new Runnable() {
-//            @Override
-//            public void run() {
-//                baseDialog.show();
-//            }
-//        };
-//        Table table = new Table();  // 创建一个表格用于布局
-//        Slider speedSlider = new Slider(1, 10, 1, false);  // 创建滑块，范围从1到10，步长为1
-//        // 将滑块和按钮添加到表格
-//        table.add(speedSlider).fillX().pad(10);  // 添加滑块，填充宽度并加一些边距
-//        // 创建 "退出测试" 按钮
-//        TextButton exitButton = new TextButton("退出测试");
-//        baseDialog.cont.add(exitButton).pad(10); // 添加退出按钮到对话框
-//        baseDialog.show();
-    }
 }
