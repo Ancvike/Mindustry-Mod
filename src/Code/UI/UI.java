0985833df89ea9ca0b943ad02dd9e3b072339503
Ancvike <2047870696@qq.com>
2024-11-23 20:38:15 +0800

    测试

--- src/Code/UI/UI.java
+++ src/Code/UI/UI.java
@@ -5,7 +5,7 @@ import arc.scene.ui.ImageButton;
 import mindustry.Vars;
 
 public class UI {
-    ImageButton imageButton = new ImageButton(Core.atlas.find("../../sprites/frog.png"));
+    ImageButton imageButton = new ImageButton(Core.atlas.find("example-java-mod-frog"));
 
     public UI() {
         Vars.ui.hudGroup.fill(t -> {
