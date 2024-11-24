98ecb02d5dd2f12c51df025fd078f5b52bba536d
Ancvike <2047870696@qq.com>
2024-11-24 20:25:28 +0800

    测试

--- src/code/ui/UI.java
+++ src/code/ui/UI.java
@@ -1,14 +1,21 @@
 package code.ui;
 
 import arc.Core;
+import arc.Events;
 import arc.scene.ui.ImageButton;
 import mindustry.Vars;
+import mindustry.content.SectorPresets;
+import mindustry.game.EventType;
 import mindustry.ui.dialogs.BaseDialog;
 
 public class UI {
     private final ImageButton imageButton = new ImageButton(Core.atlas.find("full-resource-frog"));
+    private boolean isUnlocked = false;
 
     public UI() {
+        Events.on(EventType.SectorCaptureEvent.class, e -> {
+            isUnlocked = true;
+        });
         Vars.ui.hudGroup.fill(t -> {
             t.add(imageButton).size(70, 70);
             t.top();
@@ -16,13 +23,24 @@ public class UI {
             imageButton.clicked(this::fullResource);
         });
     }
+
     public void fullResource() {
-        BaseDialog dialog = new BaseDialog("frog");
+        if (isUnlocked) {
+            BaseDialog dialog = new BaseDialog("frog");
             dialog.cont.add("behold").row();
             //mod sprites are prefixed with the mod name (this mod is called 'example-java-mod' in its config)
 //        mod名不能有大写字母
-            dialog.cont.image(Core.atlas.find("example-java-mod-frog")).pad(20f).row();
+            dialog.cont.image(Core.atlas.find("full-resource-frog")).pad(20f).row();
             dialog.cont.button("I see", dialog::hide).size(100f, 50f);
             dialog.show();
+        } else {
+            BaseDialog dialog1 = new BaseDialog("frog");
+            dialog1.cont.add("失败,未成功").row();
+            //mod sprites are prefixed with the mod name (this mod is called 'example-java-mod' in its config)
+//        mod名不能有大写字母
+            dialog1.cont.image(Core.atlas.find("full-resource-frog")).pad(20f).row();
+            dialog1.cont.button("I see", dialog1::hide).size(100f, 50f);
+            dialog1.show();
+        }
     }
 }
