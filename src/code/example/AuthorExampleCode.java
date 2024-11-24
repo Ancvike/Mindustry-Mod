352d63d6be8af88c9380e2f618fcf271e92ea3b5
Ancvike <2047870696@qq.com>
2024-11-24 11:25:41 +0800

    测试

--- src/code/example/AuthorExampleCode.java
+++ src/code/example/AuthorExampleCode.java
@@ -10,6 +10,7 @@ public class AuthorExampleCode {
 //            BaseDialog dialog = new BaseDialog("frog");
 //            dialog.cont.add("behold").row();
 //            //mod sprites are prefixed with the mod name (this mod is called 'example-java-mod' in its config)
+    //mod名不能有大写字母
 //            dialog.cont.image(Core.atlas.find("example-java-mod-frog")).pad(20f).row();
 //            dialog.cont.button("I see", dialog::hide).size(100f, 50f);
 //            dialog.show();
