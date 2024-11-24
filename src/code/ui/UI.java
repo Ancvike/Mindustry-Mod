dc9a8c0b3d1a00cc1eff180fe5c38f8a2a9ac99e
Ancvike <2047870696@qq.com>
2024-11-24 12:40:28 +0800

    测试

--- src/code/ui/UI.java
+++ src/code/ui/UI.java
@@ -1,21 +1,29 @@
 package code.ui;
 
 import arc.Core;
+import arc.scene.style.Drawable;
 import arc.scene.ui.ImageButton;
 import mindustry.Vars;
+import mindustry.ui.dialogs.BaseDialog;
 
 public class UI {
-    ImageButton imageButton = new ImageButton(Core.atlas.find("full-resource-frog"));
+    private final ImageButton imageButton = new ImageButton();
 
     public UI() {
         Vars.ui.hudGroup.fill(t -> {
-            t.add(imageButton).size(50,50);
+            t.add(imageButton).size(70, 70);
             t.top();
             t.x = 300;
+            imageButton.button((Drawable) Core.atlas.find("full-resource-frog"), this::fullResource);
         });
     }
-
-    public void onClick() {
-
+    public void fullResource() {
+        BaseDialog dialog = new BaseDialog("frog");
+            dialog.cont.add("behold").row();
+            //mod sprites are prefixed with the mod name (this mod is called 'example-java-mod' in its config)
+//        mod名不能有大写字母
+            dialog.cont.image(Core.atlas.find("example-java-mod-frog")).pad(20f).row();
+            dialog.cont.button("I see", dialog::hide).size(100f, 50f);
+            dialog.show();
     }
 }
