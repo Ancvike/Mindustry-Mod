219ecb87ac9603c9c4d3cda93241c9f212de32cb
Ancvike <2047870696@qq.com>
2024-11-25 15:42:52 +0800

    测试

--- src/FullResource/ui/UI.java
+++ src/FullResource/ui/UI.java
@@ -9,25 +9,23 @@ import static mindustry.Vars.state;
 
 public class UI {
     private final ImageButton imageButton = new ImageButton(Core.atlas.find("full-resource-frog"));
-    private BaseDialog dialog = new BaseDialog("测试");
+    BaseDialog dialog = new BaseDialog("测试");
 
     public UI() {
         Vars.ui.hudGroup.fill(t -> {
             t.add(imageButton).size(70, 70);
             t.top();
             t.x = 300;
-            if (!state.rules.waves && state.isCampaign()) {
-
-            } else {
-
-                dialog.cont.add("区块未占领,无法使用该功能").row();
-                //mod sprites are prefixed with the mod name (this mod is called 'example-java-mod' in its config)
-                //mod名不能有大写字母
-                dialog.cont.image(Core.atlas.find("full-resource-frog")).pad(20f).row();
-                dialog.cont.button("了解", dialog::hide).size(100f, 50f);
-                dialog.show();
-            }
+            dialog_show();
             imageButton.clicked(() -> dialog.show());
         });
+    }
+
+    public void dialog_show() {
+        if (state.rules.waves && !state.isCampaign()) {//区块是否占领
+            dialog.cont.add("区块未占领,无法使用该功能").row();
+            dialog.cont.image(Core.atlas.find("full-resource-frog")).pad(20f).row();
+            dialog.cont.button("了解", dialog::hide).size(100f, 50f);
+        }
     }
 }
