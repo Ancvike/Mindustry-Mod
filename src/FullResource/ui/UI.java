9a79573def30109ebaad78a4062bdb451b188179
Ancvike <2047870696@qq.com>
2024-12-16 12:29:52 +0800

    测试

--- src/FullResource/ui/UI.java
+++ src/FullResource/ui/UI.java
@@ -1,23 +1,22 @@
 package FullResource.ui;
 
-import FullResource.core.CoreWindow;
+import FullResource.test.SidebarSwitcher;
 import arc.Core;
 import arc.scene.ui.ImageButton;
+import arc.scene.ui.layout.Table;
 import mindustry.Vars;
 
 public class UI {
     public final ImageButton imageButton = new ImageButton(Core.atlas.find("full-resource-frog"));
-    public CoreWindow coreWindow = new CoreWindow();
+    public static SidebarSwitcher sidebarSwitcherFragment;
+    Table table = new Table();
     public UI() {
         Vars.ui.hudGroup.fill(t -> {
             t.add(imageButton).size(70, 70);
             t.top();
             t.x = 300;
         });
-        Vars.ui.hudGroup.fill(t -> {
-            t.name = "Windows";
-            coreWindow.build();
-            t.add(coreWindow);
-        });
+        table.add(imageButton).size(70, 70);
+        sidebarSwitcherFragment = new SidebarSwitcher(table);
     }
 }
