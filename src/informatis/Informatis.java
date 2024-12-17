53701ecd98ce5ea2e7d0f6ac47893f0e0ad1b590
Ancvike <2047870696@qq.com>
2024-12-17 13:17:02 +0800

    测试

--- src/informatis/Informatis.java
+++ src/informatis/Informatis.java
@@ -1,10 +1,13 @@
 package informatis;
 
+import informatis.core.setting.SharSettingUI;
+import informatis.core.UpdateChecker;
 import informatis.ui.fragments.sidebar.windows.tools.draws.OverDrawManager;
 import informatis.ui.fragments.sidebar.windows.tools.tools.ToolManager;
 import informatis.ui.fragments.sidebar.dialogs.DialogManager;
 import informatis.ui.fragments.sidebar.windows.*;
 import informatis.ui.fragments.FragmentManager;
+import informatis.core.ModMetadata;
 
 import mindustry.game.EventType.*;
 import mindustry.mod.*;
@@ -14,12 +17,14 @@ public class Informatis extends Mod {
     @Override
     public void init(){
         Events.on(ClientLoadEvent.class, e -> {
-            WindowManager.init();
+            ModMetadata.init();
+            SharSettingUI.init();
             DialogManager.init();
             FragmentManager.init();
             OverDrawManager.init();
             ToolManager.init();
             SVars.init();
+            UpdateChecker.init();
         });
     }
 }
